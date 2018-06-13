ba2cb7409a624f6c975eaed9bb042460d0b17ac0
Emily S <emily.sugano@gmail.com>
2018-06-13 22:14:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,12 +27,12 @@ class Dog
   end
 
   def save
-    # if self.id
-    #   self.update
-    # else
+    if self.id
+      self.update
+    else
       DB[:conn].execute('INSERT INTO dogs (name, breed) VALUES (?,?)', self.name, self.breed)
       @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
-    # end
+    end
   end
 
   def update
