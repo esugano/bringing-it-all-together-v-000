718a8da8619aaff3d04530f94369e5ffe4d0562c
Emily S <emily.sugano@gmail.com>
2018-06-13 22:47:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,4 +46,21 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
+  describe "::create" do
+    it 'takes in a hash of attributes and uses metaprogramming to create a new dog object. Then it uses the #save method to save that dog to the database'do
+      Dog.create(name: "Ralph", breed: "lab")
+      expect(DB[:conn].execute("SELECT * FROM dogs")).to eq([[1, "Ralph", "lab"]])
+    end
+    it 'returns a new dog object' do
+      dog = Dog.create(name: "Dave", breed: "podle")
+
+      expect(teddy).to be_an_instance_of(Dog)
+      expect(dog.name).to eq("Dave")
+    end
+  end
+
+  def self.create(name, breed)
+    self.new(name: name, breed:breed).save
+
+  end
 end
