1176743d5023743e17e893b4feb277c25fb1182f
Emily S <emily.sugano@gmail.com>
2018-06-13 22:16:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,14 +36,14 @@ class Dog
     end
   end
 
-  # def update
-  #   sql = <<-SQL
-  #   UPDATE dogs
-  #   SET name = ?, breed = ?
-  #   WHERE id = ?
-  #   SQL
-  #
-  #   DB[:conn].execute(sql, self.name, self.breed, self.id)
-  # end
+  def update
+    sql = <<-SQL
+    UPDATE dogs
+    SET name = ?, breed = ?
+    WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 end
