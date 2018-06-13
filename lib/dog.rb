436b4d3337637249dc84d2eb787e1cd129bcee0a
Emily S <emily.sugano@gmail.com>
2018-06-13 21:59:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,14 +10,13 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE dogs (
+    CREATE TABLE IF NOT EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
     )
     SQL
-    
-    DB[:conn].execute('DROP TABLE IF EXISTS dogs')
+
     DB[:conn].execute(sql)
   end
 end
